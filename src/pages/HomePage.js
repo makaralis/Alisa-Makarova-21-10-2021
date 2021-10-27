80eabb10749d336a626267949bc4124129fa7b71
Alisa <alisa@jonnix.co>
2021-10-28 00:27:20 +0300

    moved functions to utils folder

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,21 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
 import { Autocomplete, TextField, Typography } from "@mui/material";
 import useGeoLocation from '../hooks/useGeoLocation';
 import { AutocompleteWrapper, StyledGrid, Container } from "../styles/homePageStyles";
 import WeatherCard from "../components/WeatherCard";
 import Loading from "../components/Loading";
-import { findMyCity } from "../utils";
+import axios from "axios";
+import { toast } from "react-toastify";
+import {  days, setMyCityData, getForecast } from "../utils";
 import { useSelector, useDispatch } from "react-redux";
 import { chooseCity } from "../actions";
 import useTheme from "../hooks/useTheme";
 import useTempUnit from '../hooks/useTempUnit'
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const HomePage = () => {
     const [citiesOptions, setCitiesOptions] = useState([]);
-    const [selectCity, setSelectCity] = useState('Tel Aviv');
+    const [chosenCity, setChosenCity] = useState('');
     const [cityKey, setCityKey] = useState('');
     const [forecast, setForecast] = useState([]);
     const [currentForecast, setCurrentForecast] = useState([]);
@@ -25,48 +24,29 @@ const HomePage = () => {
     const theme = useTheme();
     const temp = useTempUnit();
 
-    const selectCityRed = useSelector((state) => {return state.chosenCityReducer});
-
+    const chosenCityRed = useSelector((state) => {return state.chosenCityReducer});
     const dispatch = useDispatch();
 
-    // const geoLocation = useGeoLocation();
+    const geoLocation = useGeoLocation();
 
-    // let longitude = 0;
-    // let latitude = 0;
-
-    // if (geoLocation.loaded && chosenCity.name === '') {
-    //     longitude = geoLocation.coordinates?.lng;
-    //     latitude = geoLocation.coordinates?.lat;
-    //     findMyCity(latitude, longitude)
-    //         .then((data) => {
-    //             setChosenCity({name: data.ParentCity.EnglishName, key: data.ParentCity.Key});
-    //             getForecast(data.ParentCity.Key);
-    //         });
-    // }
-
-    // if (geoLocation.loaded.coordinates === undefined && chosenCity.name === '') {
-    //     console.log('here');
-    //     // IF THE USER DONT WANT TO SHOW LOCATION
-    // }
-   
 
     const getCities = async () => {
         try {
             const res = await axios.get(`https://dataservice.accuweather.com/locations/v1/topcities/150?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`);
-
+    
             setCitiesOptions(res ? res.data : []);
-
-            if (selectCityRed.data.length < 1) {
+    
+            if (chosenCityRed.data.length < 1 && geoLocation.error !== undefined) {
                 const res = await axios.get(
                   `http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`
                 );
-
+    
           
                 const currentRes = await axios.get(
                   `https://dataservice.accuweather.com/currentconditions/v1/215854/?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`
                 );
           
-                dispatch(chooseCity("Tel Aviv", "215854", res.data.DailyForecasts, currentRes.data[0].WeatherText));
+               dispatch(chooseCity("Tel Aviv", "215854", res.data.DailyForecasts, currentRes.data[0].WeatherText));
               }
         }
         catch (err) {
@@ -74,77 +54,70 @@ const HomePage = () => {
         }
     }
 
-    const getForecast = async (key) => {
-        try {
-            const res = await axios.get(
-            `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&details=true`
-            );
-        
-            const currentRes = await axios.get(
-            `https://dataservice.accuweather.com/currentconditions/v1/${key}/?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&details=true`
-            );
-        
-            if (res && currentRes) {
-                setForecast(res.data.DailyForecasts);
-                setCurrentForecast(currentRes.data[0].WeatherText);
-            }
-        }
-        catch (err) {
-            toast.error('Error while getting information about forecast', toast.POSITION.BOTTOM_RIGHT);
-        }
     
-    }
 
     const handleAutocompleteChange =  (event , val) => {
-        const city = citiesOptions.find((city) => city.EnglishName === val);
+        const city = citiesOptions.find((city) => {
+            if(val) {
+                return city.EnglishName === val;
+            }
 
-        setIsCitySelected(true)
-        setSelectCity(city.EnglishName);
-        setCityKey(city.Key);
-    
-        getForecast(city.Key);
+            return null;
+        });
+
+        if (val) {
+            setIsCitySelected(true)
+            setChosenCity(city.EnglishName);
+            setCityKey(city.Key);
+        
+            getForecast(city.Key, (val) => setForecast(val), (val) => setCurrentForecast(val));
+        }
     }
 
-    useEffect(() => 
-        getCities()
-    , []);
+    useEffect( () => {
+        if (geoLocation.loaded && geoLocation.error === undefined && chosenCityRed.data.length < 1) {
+            setMyCityData(geoLocation, (val) => setIsCitySelected(val), (val) => setChosenCity(val), (val) => setCityKey(val), (val) => setForecast(val), (val) => setCurrentForecast(val));
+        }
+        
+        getCities()}
+    , );
 
     useEffect(() => {
-        if (isCitySelected) {
-          dispatch(chooseCity(selectCity, cityKey, forecast, currentForecast));
+        if (isCitySelected && chosenCity !== '') {
+          dispatch(chooseCity(chosenCity, cityKey, forecast, currentForecast));
         }
-      }, [selectCity, cityKey, forecast, currentForecast, dispatch, isCitySelected])
+      }, [chosenCity, cityKey, forecast, currentForecast, dispatch, isCitySelected])
 
 
     return (
         <Container>
-        {(citiesOptions.length === 0 ||  theme?.modeStatus === 'loading' || temp?.modeStatus === 'loading')  ? <Loading/> :
-            (<>
-                <AutocompleteWrapper>
-                    <Autocomplete
-                        disablePortal
-                        options={citiesOptions.map((item) => item.EnglishName)}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="City"/>}
-                        onChange={ handleAutocompleteChange}
-                        />
-                
-                </AutocompleteWrapper>
+            {(citiesOptions.length === 0 ||  theme?.modeStatus === 'loading' || temp?.modeStatus === 'loading')  ? <Loading/> :
+                (<>
+                    <AutocompleteWrapper>
+                        <Autocomplete
+                            disablePortal
+                            options={citiesOptions.map((item) => item.EnglishName)}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="City"/>}
+                            onChange={handleAutocompleteChange}
+                            />
+                    
+                    </AutocompleteWrapper>
 
-                {selectCityRed.forecast !== undefined && selectCityRed.currentForecast !== undefined && typeof selectCityRed.currentForecast !== 'object' ? (
-                    <> 
-                        <Typography variant='h2' sx={{marginBottom: '12px', fontWeight:'bold'}}>{selectCityRed.data}</Typography>
-                        <Typography variant='h4' sx={{marginBottom: '20px'}}>Today is  {selectCityRed.currentForecast}</Typography>
-                
-                        <StyledGrid container justifyContent='center' spacing={{ xs: 2, md: 3, lg: 6, xl: 2 }}>
-                            {selectCityRed.forecast.map((item) => 
-                            <WeatherCard minTemp={item.Temperature.Minimum.Value} maxTemp={item.Temperature.Maximum.Value}
-                            day={days[new Date(item.Date).getDay()]}/>)}
-                        </StyledGrid>
-                    </>)
-                : null}
-            </>)
-        }    
+                    {chosenCityRed.forecast !== undefined && chosenCityRed.currentForecast !== undefined && typeof chosenCityRed.currentForecast !== 'object' ? (
+                        <> 
+                            <Typography variant='h2' sx={{marginBottom: '12px', fontWeight:'bold'}}>{chosenCityRed.data}</Typography>
+                            <Typography variant='h4' sx={{marginBottom: '20px'}}>Today is  {chosenCityRed.currentForecast}</Typography>
+                    
+                            <StyledGrid container justifyContent='center' spacing={{ xs: 2, md: 3, lg: 6, xl: 2 }}>
+                                {chosenCityRed.forecast.map((item, index) => 
+                                <WeatherCard key={item.EpochDate} minTemp={item.Temperature.Minimum.Value} maxTemp={item.Temperature.Maximum.Value}
+                                day={days[new Date(item.Date).getDay()]}/>)}
+                            </StyledGrid>
+                        </>)
+                    : null}
+                </>)
+            }    
         </Container>)
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import { chooseCity } from "../actions";
 
 
-export const findMyCity = async (lat, ln) => {
+const findMyCity = async (lat, ln) => {
     try {
         const res = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&q=${lat}%2C%20${ln}`);
         
@@ -12,5 +13,67 @@ export const findMyCity = async (lat, ln) => {
         toast.error('Error while getting a current location', toast.POSITION.BOTTOM_RIGHT);
     }
 }
+
+export const setMyCityData = async (geoLocation, setIsCitySelected, setChosenCity, setCityKey, setForecast, setCurrentForecast) => {
+    try {
+        const data = await findMyCity(geoLocation.coordinates.lat, geoLocation.coordinates.lng);
+
+        if (data) {
+            setIsCitySelected(true);
+            setChosenCity(data.ParentCity?.EnglishName);
+            setCityKey(data.ParentCity?.Key);
+            await getForecast(data.ParentCity?.Key, (val) => setForecast(val), (val) => setCurrentForecast(val));
+        }
+    }
+    catch (err) {
+        toast.error('Error while getting your city data', toast.POSITION.BOTTOM_RIGHT);
+    }
+}
+
+export const getCities = async (dispatch, geoLocation, setCitiesOptions, chosenCityRed) => {
+    try {
+        const res = await axios.get(`https://dataservice.accuweather.com/locations/v1/topcities/150?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`);
+
+        setCitiesOptions(res ? res.data : []);
+
+        if (chosenCityRed.data.length < 1 && geoLocation.error !== undefined) {
+            const res = await axios.get(
+              `http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`
+            );
+
+      
+            const currentRes = await axios.get(
+              `https://dataservice.accuweather.com/currentconditions/v1/215854/?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}`
+            );
+      
+           dispatch(chooseCity("Tel Aviv", "215854", res.data.DailyForecasts, currentRes.data[0].WeatherText));
+          }
+    }
+    catch (err) {
+        toast.error('Error while getting locations', toast.POSITION.BOTTOM_RIGHT);
+    }
+}
+
+
+export const getForecast = async (key, setForecast, setCurrentForecast) => {
+    try {
+        const res = await axios.get(
+        `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${key}?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&details=true`
+        );
+    
+        const currentRes = await axios.get(
+        `https://dataservice.accuweather.com/currentconditions/v1/${key}/?apikey=${process.env.REACT_APP_ACCUWEATHER_API_KEY}&details=true`
+        );
+    
+        if (res && currentRes) {
+            setForecast(res.data.DailyForecasts);
+            setCurrentForecast(currentRes.data[0].WeatherText);
+        }
+    }
+    catch (err) {
+        toast.error('Error while getting information about forecast', toast.POSITION.BOTTOM_RIGHT);
+    }
+}
+
 
 export const  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
